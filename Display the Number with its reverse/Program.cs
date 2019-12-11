41843091352243f147c0bbcf2219bca1413cfce5
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:31:31 -0800

    Add project files.

---
+++ Display the Number with its reverse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_the_Number_with_its_reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
